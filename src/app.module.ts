ee704ccff570f315367844fdf3895340d9d5bdf0
Iulian2023 <rotaruiulian19@gmail.com>
2026-02-07 18:41:46 +0100

    add new route enclosure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SpecieModule } from './specie/specie.module';
 import { AnimalModule } from './animal/animal.module';
 import { HabitatModule } from './habitat/habitat.module';
+import { EnclosureModule } from './enclosure/enclosure.module';
 
 @Module({
-  imports: [SpecieModule, AnimalModule, HabitatModule],
+  imports: [SpecieModule, AnimalModule, HabitatModule, EnclosureModule],
   controllers: [],
   providers: [],
 })
